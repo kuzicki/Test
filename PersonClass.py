9c479bfbe90535c0805af4f433eca0594f51d2d7
kuzicki <kuzmickii1@yandex.ru>
2022-09-19 22:39:57 +0300

    Added Greeting to the class

--- PersonClass.py
+++ PersonClass.py
@@ -5,7 +5,11 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
+    def Greeting(self):
+        print("Hello, my name is " + self.name)
+
 
 p = Person("Nick", 17)
 
-print(p.name + " " + str(p.age))
+p.Greeting()
